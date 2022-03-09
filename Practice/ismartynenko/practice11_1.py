ea90f34de733903a839d3e56a3479a856c69cc63
ismartynenko <martein87@gmail.com>
2022-03-09 16:52:26 +0300

    Практика 11-1

--- Practice/ismartynenko/practice11_1.py
+++ Practice/ismartynenko/practice11_1.py
@@ -3,29 +3,29 @@ import json
 
 
 class SqlWrapper:
-    # При входе в наш менеджер контекста - создаем соединение с БД
     def __enter__(self):
-        self._db_name = "films.db"
-        self.conn = sqlite3.connect(self._db_name)
-        self.conn.row_factory = sqlite3.Row
+        _db_name = "films.db"
+        self.conn = sqlite3.connect(_db_name)
+        return self
 
-    # При выходе - завершаем соединение
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.conn.close()
 
-    # метод select
     def select(self, query):
         cur = self.conn.cursor()
         cur.execute(query)
-        return json.dumps(cur.fetchall())
+        print(json.dumps(cur.fetchall()))
 
-    # метод execute
     def execute(self, query):
         cur = self.conn.cursor()
         cur.execute(query)
         self.conn.commit()
 
 
-with SqlWrapper():
-    SqlWrapper.execute(query="INSERT INTO films (name, desc) VALUES ('Cool Film', 'SHORT LONG STORY')")
-    SqlWrapper.select(query="SELECT * FROM films")
+if __name__ == "__main__":
+    with SqlWrapper() as sqw:
+        sqw.execute("INSERT INTO films (name, desc) VALUES ('Cool Film', 'SHORT LONG STORY')")
+        sqw.select("SELECT * FROM films")
+        sqw.execute("INSERT INTO films (name, desc) VALUES ('UnCool Film', 'unSHORT LONG STORY')")
+        sqw.select("SELECT * FROM films WHERE name = 'UnCool Film'")
+
