37e58007c0f3a1661736171c8a770e8219c96a32
ismartynenko <martein87@gmail.com>
2022-03-13 19:12:42 +0300

    Экзамен 1 - Задача 10 Операции сравнения еще осталось - какие взять? > < >= <= Или еще какие-то?

--- Practice/ismartynenko/exam_10.py
+++ Practice/ismartynenko/exam_10.py
@@ -5,44 +5,62 @@ class Money:
 
         self.rub = int(self.spl[0])
         if len(self.spl[1]) == 1:
-            self.kop = float(self.spl[1] + '.0')
-        elif self.spl[1].startswith('0'):
-            self.kop = float(self.spl[1].replace('0', '0.'))
+            self.kop = int(self.spl[1]) * 10
         else:
-            self.kop = int(self.spl[1]) / 10
+            self.kop = int(self.spl[1])
 
     def exchange(self):
         _ex_rate = 116.40
-        return f"${str(round(self.num / _ex_rate, 2)).replace('.', ',')}"
+        return round(self.num / _ex_rate, 2)
 
     def __add__(self, other):
         self.sum_rub = self.rub + other.rub
         self.sum_kop = self.kop + other.kop
 
-        if self.sum_kop >= 10:
+        if self.sum_kop >= 100:
             self.sum_rub += 1
-            self.sum_kop = str(self.sum_kop - 10).replace('.', '')
-        else:
-            self.sum_kop = str(self.sum_kop).replace('.', '')
-        return f"{self.sum_rub},{self.sum_kop}"
+            self.sum_kop = self.sum_kop - 100
+        res = self.sum_rub + self.sum_kop / 100
+        return res
 
     def __sub__(self, other):
-        pass
+        self.sub_rub = self.rub - other.rub
+        self.sub_kop = self.kop - other.kop
+
+        if self.sub_kop < 0:
+            self.sub_rub -= 1
+            self.sub_kop = self.sub_kop + 100
+        res = self.sub_rub + self.sub_kop / 100
+        return res
 
     def __truediv__(self, other):
-        pass
+        res = (self.num * 100) / (other.num * 100)
+        return res
 
     def __str__(self):
-        return f"{self.rub},{int(self.kop * 10)}"
+        return f"{self.rub},{self.kop}"
+
+    def __lt__(self, other):
+        pass
+
+    def __le__(self, other):
+        pass
+
+    def __gt__(self, other):
+        pass
+
+    def __ge__(self, other):
+        pass
 
 
-s1 = Money(10000.21)
-s2 = Money(20000.99)
+s1 = Money(2500.50)
+s2 = Money(500.00)
+s3 = Money(499.50)
 
 print(s1.exchange())
 print(s2.exchange())
+print(s3.exchange())
 
-print(s1)
-print(s2)
-s = s1 + s2
-print(s)
+print(f'{s1 + s3 = }')
+print(f'{s2 - s3 = }')
+print(f'{s1 / s2 = }')
